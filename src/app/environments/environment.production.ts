d316ad3a18331c828fa675be0b6f73270ae781e0
sajan <sajan.v@benzyinfotech.com>
2023-11-09 13:44:13 +0530

    test

--- src/app/environments/environment.production.ts
+++ src/app/environments/environment.production.ts
@@ -1,3 +1,10 @@
 export const environment = {
     production: true,
+    isB2b: true,
+    RedirectionURL: "https://agents.akbartravelsonline.com/b2bplus/",
+    BASE_URL: "https://amhtransfers.benzyinfotech.com/api/",
+    utils: {
+        util: 'https://b2bstageutilsapi.benzyinfotech.com',
+        utildomain: 'b2bstageutilsapi.benzyinfotech.com',
+    }
 };
